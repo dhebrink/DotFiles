9f1db5eae39576952ff10d14df9b44269a731319
Daniel <danielhebrink@britecore.com>
2016-05-25 13:15:36 -0500

    gitconfig user updated and zshrc updated

--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -5,22 +5,34 @@ export PATH="$HOME/.venv/bin:$HOME/bin:$HOME/Documents/go-zone/bin:$(brew --pref
 export MANPATH="$(brew --prefix gnu-sed)/libexec/gnuman:$MANPATH"
 export GOPATH="$HOME/Documents/go-zone"
 export NODE_PATH="/usr/local/lib/node_modules"
+export VBOX_DISK_SIZE=100000
 
 # Homebrew cask aliases
 alias cask_install="brew cask install --appdir=/Applications"
 alias cask_uninstall="brew cask uninstall"
 alias cask_search="brew cask search"
 alias cask_list="brew cask list"
-# Set up haste to use our BriteCore server
-alias work_haste='HASTE_SERVER=http://hastebin.britecorepro.com haste'
 # Vagrant Aliases
-alias vud='cd ~/BC-Up && vagrant up dev'
-alias vhd='cd ~/BC-Up && vagrant halt dev'
-alias vsd='cd ~/BC-Up && vagrant ssh dev'
-alias vpd='cd ~/BC-Up && vagrant provision dev'
-alias vagrantrun="ssh -i ~/.vagrant.d/insecure_private_key -c none -YC -p2222 vagrant@localhost"
+alias start-vm="VBoxManage startvm BC-docker-2016-04-10 --type headless"
+alias stop-vm="VBoxManage controlvm BC-docker-2016-04-10 poweroff"
+alias enter-vm="ssh vagrant@localhost -p 2222"
 # Better ls alias for Mac
 alias ls='ls -GpFh'
 
+# My custom aliases
+alias bc='cd ~/git/BriteCore/'
+alias update_powertools='pip install -U git+ssh://git@github.com/IntuitiveWebSolutions/PowerTools.git'
+alias update_gitiws='cd && ~/.venv/bin/pip install -U git+ssh://git@github.com/IntuitiveWebSolutions/Git-IWS.git'
+alias devbrite='nocorrect devbrite '
+
 PLUGINS=('vagrant' 'brew')
 source "$ZSH_DIR/common.zsh"
+
+# custom methods
+function vimdatshit () {
+    ag -l $1 $2 | xargs -o vim +/$1
+}
+
+function work-with () {
+sequelpro $1 && ssh aws.$1-Leader
+}
